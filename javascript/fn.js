f9be003821b39a7fb6ebeba8abdc23544e3251d1
Yashraj17042006 <yashrajmaharaul@gmail.com>
2026-08-18 11:27:04 +0530

    function

--- javascript/fn.js
+++ javascript/fn.js
@@ -14,3 +14,33 @@ console.log(add(5,5));
 
 const greet = greeting();
 greet();
+
+const variable=function(){
+    console.log("Hello");
+};
+
+function name(msg){
+    return msg;
+}
+function hello(fn){
+    console.log("Hello");
+    let x=fn("Yash");
+    console.log(x);
+
+}
+hello(name);
+// high-order function 
+function highOrder(a){
+    return function(msg){
+        console.log(msg);
+    }       
+}   
+function hello(a){
+    console.log("hey");
+    console.log(a);
+}
+function exe(own){
+    console.log("exe");
+    own(2);
+}
+exe(hello);
